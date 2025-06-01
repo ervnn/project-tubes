1a82e408e160caf48f02dfa09da9befa12e78984
ervnn <ervinsetyanata5@gmail.com>
2025-06-02 02:32:58 +0700

    routing App to komentarPage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import './App.css';
+import App from './App.jsx';
+import KomentarPage from './komentar.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/komentar" element={<KomentarPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
